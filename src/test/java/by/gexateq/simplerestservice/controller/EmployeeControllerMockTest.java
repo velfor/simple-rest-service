015ccb4f226ff3e98fc715a737399ea82ab3a33b
Vlad Kuznetsov <vladimir.kuznetsov@altoros.com>
2024-03-28 10:26:56 +0100

    Another one pageable API added for Employee get

--- src/test/java/by/gexateq/simplerestservice/controller/EmployeeControllerMockTest.java
+++ src/test/java/by/gexateq/simplerestservice/controller/EmployeeControllerMockTest.java
@@ -27,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(EmployeeController.class)
 class EmployeeControllerMockTest {
 
+    @SuppressWarnings("unused")
     @Autowired
     private MockMvc mockMvc;
 
